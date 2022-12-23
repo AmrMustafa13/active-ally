d79f6ad5b976f1011a3d66080a930db6f56414ca
AmrMustafa13 <amrmostafa1311@gmail.com>
2022-12-23 13:56:00 +0200

    signup logic and validation

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,12 +1,53 @@
-const User = require("../models/user");
+const userSchema = require("../models/user");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
 
-const loginUser = (req, res) => {
+const createToken = (_id) => {
+  const token = jwt.sign({ _id }, process.env.TOKEN_SECRET, {
+    expiresIn: "3d",
+  });
+
+  return token;
+};
+
+const loginUser = async (req, res) => {
   res.json("Login");
 };
 
-const signupUser = (req, res) => {
-  res.json("Signup");
+const signupUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  // inputs validation
+  if (!email || !password)
+    return res.status(400).json({ error: "All fields must be filled" });
+
+  if (!validator.isEmail(email))
+    return res.status(400).json({ error: "Email is not valid" });
+
+  if (!validator.isStrongPassword(password))
+    return res.status(400).json({ error: "Password is not strong enough" });
+
+  // check if email already exists
+  const exists = await userSchema.findOne({ email });
+  if (exists) return res.status(400).json({ error: "Email already exists" });
+
+  // hash the password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // saving the user to the database
+  try {
+    const newSavedUser = await userSchema.create({
+      email,
+      password: hashedPassword,
+    });
+    const token = createToken(newSavedUser._id);
+    res.status(201).json(token);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = {
