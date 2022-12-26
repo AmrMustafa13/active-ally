e4cf4638363ebdf5af0bd4881924f538b3c3769e
AmrMustafa13 <amrmostafa1311@gmail.com>
2022-12-27 01:20:48 +0200

    usesignup hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
 import "./app.css";
 
@@ -11,6 +13,8 @@ const App = () => {
       <div className="pages">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
     </div>
